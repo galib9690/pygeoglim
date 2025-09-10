85806b3288d23728e871eca5315008c99b029d88
Mohammad Galib <mgalib@mohammads-MacBook-Air.local>
2025-09-10 12:30:21 -0400

    Bump version to 1.0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pygeoglim",
-    version="1.0.0",
+    version="1.0.1",
     author="Mohammad Galib",
     description="Fast access to GLiM and GLHYMPS geology attributes for watersheds",
     long_description=long_description,
