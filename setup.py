8acb4b3cf8be25c66545f79feb3bec60552b8394
Mohammad Galib <mgalib@mohammads-MacBook-Air.local>
2025-09-19 17:54:00 -0400

    Publish pygeoglim v1.0.5 with fixed README

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pygeoglim",
-    version="1.0.4",
+    version="1.0.5",
     author="Mohammad Galib",
     description="A Python package for extracting geological attributes from GLiM and GLHYMPS datasets",
     long_description=long_description,
